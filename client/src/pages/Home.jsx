cfd2697f31801a6206e56327862691ab45383d62
Mr Sharafdin <isasharafdin@gmail.com>
2023-09-07 17:50:46 +0300

    changed the system

--- client/src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-const Home = () => {
-  return (
-    <h1 className="p-4 text-xl">You are successfully logged in and now you are a full stack developer 
-    that uses Mern techniques.🤩</h1>
-  )
-}
-
-export default Home
